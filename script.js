904837cdb077d30ae5c7a7133215b8e78adf264b
davalavil <47474842+davalavil@users.noreply.github.com>
2025-04-16 07:31:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const rooms = {
         message: 'Estás en un sótano húmedo y oscuro. Huele a tierra mojada y... a algo más. Hay una puerta de madera al fondo.',
         backgroundMusic: bgmCreepy,
         objects: [
-            { id: 'puerta_madera_sotano', name: 'Puerta de madera', coords: { top: '30%', left: '45%', width: '12%', height: '40%' },
+            { id: 'puerta_madera_sotano', name: 'Puerta de madera', coords: { top: '30%', left: '40%', width: '12%', height: '37%' },
                 action: (state) => {
                     if (state.flags.sotano_puerta_abierta) {
                         playSound(sfxDoorCreak);
