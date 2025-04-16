8528c18d68abd8b39c846ad5f46c344fd6f5cfe4
davalavil <47474842+davalavil@users.noreply.github.com>
2025-04-16 07:58:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ const rooms = {
                     }
                 }
             },
-            { id: 'caja_rota', name: 'Caja rota', coords: { top: '65%', left: '25%', width: '10%', height: '10%' },
+            { id: 'caja_rota', name: 'Caja rota', coords: { top: '68%', left: '28%', width: '10%', height: '10%' },
                 action: (state) => {
                     if (!state.flags.sotano_caja_revisada) {
                         setMessage('Remueves unos tablones sueltos y encuentras una vieja llave oxidada.');
