b3b2e7c3c40527a97b5992f4ef969786d62d2037
davalavil <47474842+davalavil@users.noreply.github.com>
2025-04-16 09:55:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -133,7 +133,7 @@ const rooms = {
                      playSound(sfxLocked);
                 }
              },
-             { id: 'volver_sotano', name: 'Puerta al Sótano', coords: { top: '55%', left: '18%', width: '2%', height: '8%' }, // Asumiendo que está a la izquierda
+             { id: 'volver_sotano', name: 'Puerta al Sótano', coords: { top: '57%', left: '17%', width: '2%', height: '6%' }, // Asumiendo que está a la izquierda
                 action: (state) => {
                     playSound(sfxDoorCreak);
                     changeRoom('sotano');
